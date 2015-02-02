06080f7ed00633249bf2d7e8d704e0e60b720cee
Tim Knutsen <timknutsen@gmail.com>
2015-02-02 14:36:22 +0100

    main function cleanup
    Fewer arguments. Ticks now at middle of chrom.
    Not certain if function works with ,... but will test.

--- gg.manhattan.R
+++ gg.manhattan.R
@@ -1,87 +1,118 @@
-gg.manhattan = function(dataframe, title=NULL, max.y="max", suggestiveline=1, genomewideline=-log10(5e-8), 
-								size.x.labels=9, size.y.labels=10, annotate=F, SNPlist=NULL, logp = TRUE) {
-	if(!require(ggplot2)) install.packages("ggplot2")
-	if(!require(dplyr)) install.packages("dplyr")
-	if (annotate & is.null(SNPlist)) stop("You requested annotation but provided no SNPlist!")
-	d=arrange(dataframe, CHR, BP)
-	#limit to chroms provided with function argument? if (bla bla)
-	#d=d[d$CHR %in% 1:29, ] # limit to bovine autosomes?
-	if ("CHR" %in% names(d) & "BP" %in% names(d) & "P" %in% names(d) ) {
-		d=na.omit(d)
-		#d=dplyr::filter(d, P > 0 & P <= 1)
-		if (logp) d = dplyr::mutate(d,logp = -log10(P)) else d = dplyr::mutate(d, logp = P)
-		d$pos=NA
-		ticks=NULL
+manhattan <- function(x, chr="CHR", bp="BP", p="P", snp="SNP", 
+							 cols=c("gray10", "gray60"), chrlabs=NULL, title= NULL,
+							 suggestiveline=-log10(1e-5), genomewideline=-log10(5e-8), 
+							 highlight=NULL, logp=TRUE, annotatePval = NULL, annotateTop = TRUE, ...) {
+	
+	# Not sure why, but package check will warn without this.
+	CHR=BP=P=index=NULL
+	
+	# Check for sensible dataset
+	## Make sure you have chr, bp and p columns.
+	if (!(chr %in% names(x))) stop(paste("Column", chr, "not found!"))
+	if (!(bp %in% names(x))) stop(paste("Column", bp, "not found!"))
+	if (!(p %in% names(x))) stop(paste("Column", p, "not found!"))
+	## warn if you don't have a snp column
+	if (!(snp %in% names(x))) warning(paste("No SNP column found. OK unless you're trying to highlight."))
+	## make sure chr, bp, and p columns are numeric.
+	if (!is.numeric(x[[chr]])) stop(paste(chr, "column should be numeric. Do you have 'X', 'Y', 'MT', etc? If so change to numbers and try again."))
+	if (!is.numeric(x[[bp]])) stop(paste(bp, "column should be numeric."))
+	if (!is.numeric(x[[p]])) stop(paste(p, "column should be numeric."))
+	
+	# Create a new data.frame with columns called CHR, BP, and P.
+	d=data.frame(CHR=x[[chr]], BP=x[[bp]], P=x[[p]])
+	
+	# If the input data frame has a SNP column, add it to the new data frame you're creating.
+	if (!is.null(x[[snp]])) d=transform(d, SNP=x[[snp]])
+	
+	# Set positions, ticks, and labels for plotting
+	## Sort and keep only values where is numeric.
+	#d <- subset(d[order(d$CHR, d$BP), ], (P>0 & P<=1 & is.numeric(P)))
+	d <- subset(d, (is.numeric(CHR) & is.numeric(BP) & is.numeric(P)))
+	d <- d[order(d$CHR, d$BP), ]
+	#d$logp <- ifelse(logp, yes=-log10(d$P), no=d$P)
+	if (logp) {
+		d$logp <- -log10(d$P)
+	} else {
+		d$logp <- d$P
+	}
+	d$pos=NA
+	
+	
+	# Fixes the bug where one chromosome is missing by adding a sequential index column.
+	d$index=NA
+	ind = 0
+	for (i in unique(d$CHR)){
+		ind = ind + 1
+		d[d$CHR==i,]$index = ind
+	}
+	
+	# This section sets up positions and ticks. Ticks should be placed in the
+	# middle of a chromosome. The a new pos column is added that keeps a running
+	# sum of the positions of each successive chromsome. For example:
+	# chr bp pos
+	# 1   1  1
+	# 1   2  2
+	# 2   1  3
+	# 2   2  4
+	# 3   1  5
+	nchr = length(unique(d$CHR))
+	if (nchr==1) { ## For a single chromosome
+		## Uncomment the next two linex to plot single chr results in Mb
+		#options(scipen=999)
+		d$pos=d$BP/1e6
+		d$pos=d$BP
+		ticks=floor(length(d$pos))/2+1
+		xlabel = paste('Chromosome',unique(d$CHR),'position')
+		labs = ticks
+	} else { ## For multiple chromosomes
 		lastbase=0
-		numchroms=nrow(dplyr::distinct(d, CHR))
-		if (numchroms==1) {
-			d$pos=d$BP
-		} else {
-			for (i in unique(d$CHR)) {
-				if (i==1) {
-					d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP
-				}   else {
-					lastbase=lastbase+tail(subset(d,CHR==i-1)$BP, 1)
-					d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP+lastbase
-				}
-				ticks=c(ticks, d[d$CHR==i, ]$pos[floor(length(d[d$CHR==i, ]$pos)/2)+1])
+		ticks=NULL
+		for (i in unique(d$index)) {
+			if (i==1) {
+				d[d$index==i, ]$pos=d[d$index==i, ]$BP
+			} else {
+				lastbase=lastbase+tail(subset(d,index==i-1)$BP, 1)
+				d[d$index==i, ]$pos=d[d$index==i, ]$BP+lastbase
 			}
-			ticklim=c(min(d$pos),max(d$pos))
-			
+			# Old way: assumes SNPs evenly distributed
+			# ticks=c(ticks, d[d$index==i, ]$pos[floor(length(d[d$index==i, ]$pos)/2)+1])
+			# New way: doesn't make that assumption
+			ticks = c(ticks, (min(d[d$CHR == i,]$pos) + max(d[d$CHR == i,]$pos))/2 + 1)
 		}
-		mycols=rep(c("gray10","gray60"),(max(d$CHR)/2)+1)
-		#mycols=rep(brewer.pal(3, "Dark2"), max(numchroms))
-		if (max.y=="max") maxy=ceiling(max(d$logp)) else maxy=max.y
-		#if (maxy<8) maxy=8 # disable to work with F-values
-		# if (annotate) d.annotate=d[as.numeric(substr(d$SNP,3,100)) %in% SNPlist, ]
+		xlabel = 'Chromosome'
+		#labs = append(unique(d$CHR),'') ## I forgot what this was here for... if seems to work, remove.
+		labs <- unique(d$CHR)
+	}
+	
+	# Initialize plot
+	xmax = ceiling(max(d$pos) * 1.03)
+	xmin = floor(max(d$pos) * -0.03)
+	ylim = ylim(c(floor(min(d$logp)),ceiling(max(d$logp))))
+	mycols = rep(cols, nchr/2+1)
+	
 		if (numchroms==1) {
 			plot=qplot(pos/1000000,logp,data=d,ylab=expression(-log[10](italic(p))), 
-						  xlab=paste("Chromosome",unique(d$CHR),"position", "Mbp"))
+						  xlab=paste(xlabel,unique(d$CHR),"position", "Mbp"))
 		}   else {
-			plot=ggplot(d, aes(x = pos,y = logp)) + geom_point(aes(colour=factor(CHR))) + 
-				ylab(expression(-log[10](italic(p))))
-			plot=plot+scale_x_continuous(name="Chromosome", breaks=ticks, labels=(unique(d$CHR)))
+			plot=ggplot(d, aes(x = pos,y = logp)) 
+			plot=plot + geom_point(aes(colour=factor(CHR))) + ylab(expression(-log[10](italic(p))))
+			plot=plot+scale_x_continuous(name=xlabel, breaks=ticks, labels=(unique(d$CHR)))
 			if (logp) plot=plot+scale_y_continuous(breaks=1:maxy+1, labels=1:maxy+1)
 			plot=plot+scale_colour_manual(values=mycols)
 		}
 		#if (annotate)   plot=plot + geom_point(data=d.annotate, colour=I("green3")) 
-		plot=plot + theme(legend.position = "none") 
 		if (is.character(title)) plot=plot + ggtitle(title)
 		plot=plot + theme(
 			panel.background=element_blank(), 
 			panel.grid.minor=element_blank(),
 			axis.line=element_line(),
-			axis.line.x=element_blank()
-			
-		)
+			axis.line.x=element_blank(),
+			legend.position = "none"
+		) + ylim
 		if (suggestiveline) plot=plot+geom_hline(yintercept=suggestiveline,colour="blue", alpha=I(1/3))
 		if (genomewideline) plot=plot+geom_hline(yintercept=genomewideline,colour="red")
 		plot
 	}   else {
 		stop("Make sure your data frame contains columns CHR, BP, and P")
 	}
-}
-
-## QQ plot using ggplot2
-gg.qq = function(pvector, title=NULL, spartan=F) {
-	library(ggplot2)
-	o = -log10(sort(pvector,decreasing=F))
-	#e = -log10( 1:length(o)/length(o) )
-	e = -log10( ppoints(length(pvector) ))
-	plot=qplot(e,o, xlim=c(0,max(e)), ylim=c(0,max(o))) + stat_abline(intercept=0,slope=1, col="red")
-	plot=plot+ggtitle(title)
-	plot=plot+scale_x_continuous(name=expression(Expected~~-log[10](italic(p))))
-	plot=plot+scale_y_continuous(name=expression(Observed~~-log[10](italic(p))))
-	if (spartan) plot=plot+opts(panel.background=theme_rect(col="grey50"), panel.grid.minor=theme_blank())
-	plot
-}
-
-## Regional plot to PDF. Unfinished code
-plot_reg <- function(dataframe, chrom, saveplot = FALSE){
-	p1 <- gg.manhattan(filter(dataframe, CHR == chrom), genomewideline = F) + 
-		geom_point(aes(color = trait)) + theme(legend.position = "right") + 
-		scale_colour_brewer(palette="Set1")
-	if (saveplot) ggsave(filename = sprintf("QTL_plot_chrom_%s.pdf", chrom), 
-								plot = p1, paper = "USr")
-	p1
 }
